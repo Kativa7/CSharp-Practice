781cdd57b2fdedbcdbc61bf6c6c394620dfe5ebf
Katerina Ilieva <61324205+Kativa7@users.noreply.github.com>
2021-04-07 14:04:28 +0300

    Add files via upload

---
+++ Area-Of-Figures/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Area_Of_Figures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            if(type == "square")
+            {
+                double side = double.Parse(Console.ReadLine());
+                Console.WriteLine(Math.Round(side * side, 3));
+            }
+            else if(type == "rectangle")
+            {
+                double sideA = double.Parse(Console.ReadLine());
+                double sideB = double.Parse(Console.ReadLine());
+                Console.WriteLine(Math.Round(sideA * sideB, 3));
+
+            }
+            else if(type == "circle")
+            {
+
+                double radius = double.Parse(Console.ReadLine());
+                Console.WriteLine(Math.Round((Math.PI * (radius * radius)), 3));
+            }
+            else if(type == "triangle")
+            {
+                double sideA = double.Parse(Console.ReadLine());
+                double h = double.Parse(Console.ReadLine());
+                Console.WriteLine(Math.Round((sideA * h)/2, 3));
+
+            }
+        }
+    }
+}
